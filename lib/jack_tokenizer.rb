ad35cf589057eddfcb67a509eebfcdb5661c6b77
wildmaples <maple.ong@shopify.com>
2021-11-03 12:26:15 -0400

    Start new implementation and implement #has_more_tokens?

--- lib/jack_tokenizer.rb
+++ lib/jack_tokenizer.rb
@@ -1,53 +1,34 @@
 class JackTokenizer
   def initialize(io)
-    @raw_tokens = io.read.split(/\s(?=(?:[^"]|"[^"]*")*$)/)
+    @input = io.read
+    @index = 0
   end
 
   def has_more_tokens?
-    !@raw_tokens.empty?
+    @index < @input.length
   end
 
   def advance
-    @current_token = @raw_tokens.shift
   end
 
   KEYWORD_TOKENS = %w[class method function constructor int boolean char void var static field let do if else while return true false null this]
   SYMBOL_TOKENS =  %w[{ } ( ) [ ] . , ; + - * / & | < > = ~]
 
   def token_type
-    begin
-      Integer(@current_token)
-      return :INT_CONST
-    rescue ArgumentError; end
-
-    if @current_token.start_with?('"')
-      :STRING_CONST
-    elsif SYMBOL_TOKENS.include?(@current_token)
-      :SYMBOL
-    elsif KEYWORD_TOKENS.include?(@current_token)
-      :KEYWORD
-    elsif @current_token.is_a?(String)
-      :IDENTIFIER
-    end
   end
 
   def key_word
-    @current_token.upcase.to_sym
   end
 
   def symbol
-    @current_token
   end
 
   def identifier
-    @current_token
   end
 
   def int_val
-    Integer(@current_token)
   end
 
   def string_val
-    @current_token[1..-2]
   end
 end
--- test/unit/jack_tokenizer_test.rb
+++ test/unit/jack_tokenizer_test.rb
@@ -14,184 +14,184 @@ class JackTokenizerTest < Minitest::Test
     assert(jack_tokenizer.has_more_tokens?)
   end
 
-  def test_advance_to_end_of_file
-    io = StringIO.new("class Foo { }")
-    jack_tokenizer = JackTokenizer.new(io)
-    4.times do
-      jack_tokenizer.has_more_tokens?
-      jack_tokenizer.advance
-    end
-    refute(jack_tokenizer.has_more_tokens?)
-  end
+  # def test_advance_to_end_of_file
+  #   io = StringIO.new("class Foo { }")
+  #   jack_tokenizer = JackTokenizer.new(io)
+  #   4.times do
+  #     jack_tokenizer.has_more_tokens?
+  #     jack_tokenizer.advance
+  #   end
+  #   refute(jack_tokenizer.has_more_tokens?)
+  # end
 
-  def test_token_type_for_symbol
-    io = StringIO.new("{")
-    jack_tokenizer = JackTokenizer.new(io)
-    jack_tokenizer.has_more_tokens?
-    jack_tokenizer.advance
-    assert_equal(:SYMBOL, jack_tokenizer.token_type)
-  end
+  # def test_token_type_for_symbol
+  #   io = StringIO.new("{")
+  #   jack_tokenizer = JackTokenizer.new(io)
+  #   jack_tokenizer.has_more_tokens?
+  #   jack_tokenizer.advance
+  #   assert_equal(:SYMBOL, jack_tokenizer.token_type)
+  # end
 
-  def test_token_type_for_another_symbol
-    io = StringIO.new("~")
-    jack_tokenizer = JackTokenizer.new(io)
-    jack_tokenizer.has_more_tokens?
-    jack_tokenizer.advance
-    assert_equal(:SYMBOL, jack_tokenizer.token_type)
-  end
+  # def test_token_type_for_another_symbol
+  #   io = StringIO.new("~")
+  #   jack_tokenizer = JackTokenizer.new(io)
+  #   jack_tokenizer.has_more_tokens?
+  #   jack_tokenizer.advance
+  #   assert_equal(:SYMBOL, jack_tokenizer.token_type)
+  # end
 
-  def test_token_type_for_key_word
-    io = StringIO.new("class")
-    jack_tokenizer = JackTokenizer.new(io)
-    jack_tokenizer.has_more_tokens?
-    jack_tokenizer.advance
-    assert_equal(:KEYWORD, jack_tokenizer.token_type)
-  end
+  # def test_token_type_for_key_word
+  #   io = StringIO.new("class")
+  #   jack_tokenizer = JackTokenizer.new(io)
+  #   jack_tokenizer.has_more_tokens?
+  #   jack_tokenizer.advance
+  #   assert_equal(:KEYWORD, jack_tokenizer.token_type)
+  # end
 
-  def test_token_type_for_another_key_word
-    io = StringIO.new("static")
-    jack_tokenizer = JackTokenizer.new(io)
-    jack_tokenizer.has_more_tokens?
-    jack_tokenizer.advance
-    assert_equal(:KEYWORD, jack_tokenizer.token_type)
-  end
+  # def test_token_type_for_another_key_word
+  #   io = StringIO.new("static")
+  #   jack_tokenizer = JackTokenizer.new(io)
+  #   jack_tokenizer.has_more_tokens?
+  #   jack_tokenizer.advance
+  #   assert_equal(:KEYWORD, jack_tokenizer.token_type)
+  # end
 
-  def test_token_type_for_identifiers
-    io = StringIO.new("FooBar")
-    jack_tokenizer = JackTokenizer.new(io)
-    jack_tokenizer.has_more_tokens?
-    jack_tokenizer.advance
-    assert_equal(:IDENTIFIER, jack_tokenizer.token_type)
-  end
+  # def test_token_type_for_identifiers
+  #   io = StringIO.new("FooBar")
+  #   jack_tokenizer = JackTokenizer.new(io)
+  #   jack_tokenizer.has_more_tokens?
+  #   jack_tokenizer.advance
+  #   assert_equal(:IDENTIFIER, jack_tokenizer.token_type)
+  # end
 
-  def test_token_type_for_identifiers_with_special_chars
-    io = StringIO.new("Foo_Bar1")
-    jack_tokenizer = JackTokenizer.new(io)
-    jack_tokenizer.has_more_tokens?
-    jack_tokenizer.advance
-    assert_equal(:IDENTIFIER, jack_tokenizer.token_type)
-  end
+  # def test_token_type_for_identifiers_with_special_chars
+  #   io = StringIO.new("Foo_Bar1")
+  #   jack_tokenizer = JackTokenizer.new(io)
+  #   jack_tokenizer.has_more_tokens?
+  #   jack_tokenizer.advance
+  #   assert_equal(:IDENTIFIER, jack_tokenizer.token_type)
+  # end
 
-  def test_token_type_for_int_values
-    io = StringIO.new("999")
-    jack_tokenizer = JackTokenizer.new(io)
-    jack_tokenizer.has_more_tokens?
-    jack_tokenizer.advance
-    assert_equal(:INT_CONST, jack_tokenizer.token_type)
-  end
+  # def test_token_type_for_int_values
+  #   io = StringIO.new("999")
+  #   jack_tokenizer = JackTokenizer.new(io)
+  #   jack_tokenizer.has_more_tokens?
+  #   jack_tokenizer.advance
+  #   assert_equal(:INT_CONST, jack_tokenizer.token_type)
+  # end
 
-  def test_token_type_for_string_values
-    io = StringIO.new('"a"')
-    jack_tokenizer = JackTokenizer.new(io)
-    jack_tokenizer.has_more_tokens?
-    jack_tokenizer.advance
-    assert_equal(:STRING_CONST, jack_tokenizer.token_type)
-  end
+  # def test_token_type_for_string_values
+  #   io = StringIO.new('"a"')
+  #   jack_tokenizer = JackTokenizer.new(io)
+  #   jack_tokenizer.has_more_tokens?
+  #   jack_tokenizer.advance
+  #   assert_equal(:STRING_CONST, jack_tokenizer.token_type)
+  # end
 
-  def test_token_type_for_string_values_with_spaces
-    io = StringIO.new('" a "')
-    jack_tokenizer = JackTokenizer.new(io)
-    jack_tokenizer.has_more_tokens?
-    jack_tokenizer.advance
-    assert_equal(:STRING_CONST, jack_tokenizer.token_type)
-  end
+  # def test_token_type_for_string_values_with_spaces
+  #   io = StringIO.new('" a "')
+  #   jack_tokenizer = JackTokenizer.new(io)
+  #   jack_tokenizer.has_more_tokens?
+  #   jack_tokenizer.advance
+  #   assert_equal(:STRING_CONST, jack_tokenizer.token_type)
+  # end
 
-  def test_string_val_returns_string_token
-    io = StringIO.new('"a"')
-    jack_tokenizer = JackTokenizer.new(io)
-    jack_tokenizer.has_more_tokens?
-    jack_tokenizer.advance
-    assert_equal(:STRING_CONST, jack_tokenizer.token_type)
-    assert_equal("a", jack_tokenizer.string_val)
-  end
+  # def test_string_val_returns_string_token
+  #   io = StringIO.new('"a"')
+  #   jack_tokenizer = JackTokenizer.new(io)
+  #   jack_tokenizer.has_more_tokens?
+  #   jack_tokenizer.advance
+  #   assert_equal(:STRING_CONST, jack_tokenizer.token_type)
+  #   assert_equal("a", jack_tokenizer.string_val)
+  # end
 
-  def test_string_val_returns_string_token_with_spaces
-    io = StringIO.new('"a foo bar"')
-    jack_tokenizer = JackTokenizer.new(io)
-    jack_tokenizer.has_more_tokens?
-    jack_tokenizer.advance
-    assert_equal(:STRING_CONST, jack_tokenizer.token_type)
-    assert_equal("a foo bar", jack_tokenizer.string_val)
-  end
+  # def test_string_val_returns_string_token_with_spaces
+  #   io = StringIO.new('"a foo bar"')
+  #   jack_tokenizer = JackTokenizer.new(io)
+  #   jack_tokenizer.has_more_tokens?
+  #   jack_tokenizer.advance
+  #   assert_equal(:STRING_CONST, jack_tokenizer.token_type)
+  #   assert_equal("a foo bar", jack_tokenizer.string_val)
+  # end
 
-  def test_string_val_returns_string_token_with_spaces_2
-    io = StringIO.new('" a foo bar"')
-    jack_tokenizer = JackTokenizer.new(io)
-    jack_tokenizer.has_more_tokens?
-    jack_tokenizer.advance
-    assert_equal(:STRING_CONST, jack_tokenizer.token_type)
-    assert_equal(" a foo bar", jack_tokenizer.string_val)
-  end
+  # def test_string_val_returns_string_token_with_spaces_2
+  #   io = StringIO.new('" a foo bar"')
+  #   jack_tokenizer = JackTokenizer.new(io)
+  #   jack_tokenizer.has_more_tokens?
+  #   jack_tokenizer.advance
+  #   assert_equal(:STRING_CONST, jack_tokenizer.token_type)
+  #   assert_equal(" a foo bar", jack_tokenizer.string_val)
+  # end
 
-  def test_string_val_returns_string_token_with_spaces_3
-    io = StringIO.new('"a foo bar "')
-    jack_tokenizer = JackTokenizer.new(io)
-    jack_tokenizer.has_more_tokens?
-    jack_tokenizer.advance
-    assert_equal(:STRING_CONST, jack_tokenizer.token_type)
-    assert_equal("a foo bar ", jack_tokenizer.string_val)
-  end
+  # def test_string_val_returns_string_token_with_spaces_3
+  #   io = StringIO.new('"a foo bar "')
+  #   jack_tokenizer = JackTokenizer.new(io)
+  #   jack_tokenizer.has_more_tokens?
+  #   jack_tokenizer.advance
+  #   assert_equal(:STRING_CONST, jack_tokenizer.token_type)
+  #   assert_equal("a foo bar ", jack_tokenizer.string_val)
+  # end
 
-  def test_string_val_returns_string_token_with_spaces_4
-    io = StringIO.new('" a foo bar "')
-    jack_tokenizer = JackTokenizer.new(io)
-    jack_tokenizer.has_more_tokens?
-    jack_tokenizer.advance
-    assert_equal(:STRING_CONST, jack_tokenizer.token_type)
-    assert_equal(" a foo bar ", jack_tokenizer.string_val)
-  end
+  # def test_string_val_returns_string_token_with_spaces_4
+  #   io = StringIO.new('" a foo bar "')
+  #   jack_tokenizer = JackTokenizer.new(io)
+  #   jack_tokenizer.has_more_tokens?
+  #   jack_tokenizer.advance
+  #   assert_equal(:STRING_CONST, jack_tokenizer.token_type)
+  #   assert_equal(" a foo bar ", jack_tokenizer.string_val)
+  # end
 
-  def test_string_val_returns_string_token_with_spaces_5
-    io = StringIO.new('" a foo    bar "')
-    jack_tokenizer = JackTokenizer.new(io)
-    jack_tokenizer.has_more_tokens?
-    jack_tokenizer.advance
-    assert_equal(:STRING_CONST, jack_tokenizer.token_type)
-    assert_equal(" a foo    bar ", jack_tokenizer.string_val)
-  end
+  # def test_string_val_returns_string_token_with_spaces_5
+  #   io = StringIO.new('" a foo    bar "')
+  #   jack_tokenizer = JackTokenizer.new(io)
+  #   jack_tokenizer.has_more_tokens?
+  #   jack_tokenizer.advance
+  #   assert_equal(:STRING_CONST, jack_tokenizer.token_type)
+  #   assert_equal(" a foo    bar ", jack_tokenizer.string_val)
+  # end
 
-  def test_int_val_returns_integer
-    io = StringIO.new("999")
-    jack_tokenizer = JackTokenizer.new(io)
-    jack_tokenizer.has_more_tokens?
-    jack_tokenizer.advance
-    jack_tokenizer.token_type
-    assert_equal(999, jack_tokenizer.int_val)
-  end
+  # def test_int_val_returns_integer
+  #   io = StringIO.new("999")
+  #   jack_tokenizer = JackTokenizer.new(io)
+  #   jack_tokenizer.has_more_tokens?
+  #   jack_tokenizer.advance
+  #   jack_tokenizer.token_type
+  #   assert_equal(999, jack_tokenizer.int_val)
+  # end
 
-  def test_identifier_returns_string_identifier
-    io = StringIO.new("FooBar")
-    jack_tokenizer = JackTokenizer.new(io)
-    jack_tokenizer.has_more_tokens?
-    jack_tokenizer.advance
-    jack_tokenizer.token_type
-    assert_equal("FooBar", jack_tokenizer.identifier)
-  end
+  # def test_identifier_returns_string_identifier
+  #   io = StringIO.new("FooBar")
+  #   jack_tokenizer = JackTokenizer.new(io)
+  #   jack_tokenizer.has_more_tokens?
+  #   jack_tokenizer.advance
+  #   jack_tokenizer.token_type
+  #   assert_equal("FooBar", jack_tokenizer.identifier)
+  # end
 
-  def test_identifier_returns_string_identifier_special_characters
-    io = StringIO.new("Foo_Bar1")
-    jack_tokenizer = JackTokenizer.new(io)
-    jack_tokenizer.has_more_tokens?
-    jack_tokenizer.advance
-    jack_tokenizer.token_type
-    assert_equal("Foo_Bar1", jack_tokenizer.identifier)
-  end
+  # def test_identifier_returns_string_identifier_special_characters
+  #   io = StringIO.new("Foo_Bar1")
+  #   jack_tokenizer = JackTokenizer.new(io)
+  #   jack_tokenizer.has_more_tokens?
+  #   jack_tokenizer.advance
+  #   jack_tokenizer.token_type
+  #   assert_equal("Foo_Bar1", jack_tokenizer.identifier)
+  # end
 
-  def test_symbol_returns_symbol
-    io = StringIO.new("~")
-    jack_tokenizer = JackTokenizer.new(io)
-    jack_tokenizer.has_more_tokens?
-    jack_tokenizer.advance
-    jack_tokenizer.token_type
-    assert_equal("~", jack_tokenizer.symbol)
-  end
+  # def test_symbol_returns_symbol
+  #   io = StringIO.new("~")
+  #   jack_tokenizer = JackTokenizer.new(io)
+  #   jack_tokenizer.has_more_tokens?
+  #   jack_tokenizer.advance
+  #   jack_tokenizer.token_type
+  #   assert_equal("~", jack_tokenizer.symbol)
+  # end
 
-  def test_symbol_returns_key_world
-    io = StringIO.new("class")
-    jack_tokenizer = JackTokenizer.new(io)
-    jack_tokenizer.has_more_tokens?
-    jack_tokenizer.advance
-    jack_tokenizer.token_type
-    assert_equal(:CLASS, jack_tokenizer.key_word)
-  end
+  # def test_symbol_returns_key_world
+  #   io = StringIO.new("class")
+  #   jack_tokenizer = JackTokenizer.new(io)
+  #   jack_tokenizer.has_more_tokens?
+  #   jack_tokenizer.advance
+  #   jack_tokenizer.token_type
+  #   assert_equal(:CLASS, jack_tokenizer.key_word)
+  # end
 end
