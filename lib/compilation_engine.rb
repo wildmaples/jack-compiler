0e125aa323b955862390fc1d09116d8c1e54c5ee
wildmaples <maple.ong@shopify.com>
2021-12-09 18:11:58 -0500

    Reduce noise for advancing through tokens

--- lib/compilation_engine.rb
+++ lib/compilation_engine.rb
@@ -6,23 +6,23 @@ class CompilationEngine
 
   def compile_class
     @output.puts("<class>")
-    @tokenizer.has_more_tokens? && @tokenizer.advance
+    advance
     @output.puts("<keyword> class </keyword>")
 
-    @tokenizer.has_more_tokens? && @tokenizer.advance
+    advance
     @output.puts("<identifier> #{@tokenizer.identifier} </identifier>")
 
-    @tokenizer.has_more_tokens? && @tokenizer.advance
+    advance
     @output.puts("<symbol> #{@tokenizer.symbol} </symbol>")
 
-    @tokenizer.has_more_tokens? && @tokenizer.advance
+    advance
     if [:STATIC, :FIELD].include?(@tokenizer.key_word)
       compile_class_var_dec
     elsif [:CONSTRUCTOR, :FUNCTION, :METHOD, :VOID].include?(@tokenizer.key_word)
       compile_subroutine
     end
 
-    @tokenizer.has_more_tokens? && @tokenizer.advance
+    advance
     @output.puts("<symbol> #{@tokenizer.symbol} </symbol>")
     @output.puts("</class>")
   end
@@ -31,13 +31,13 @@ class CompilationEngine
     @output.puts("<classVarDec>")
     @output.puts("<keyword> #{@tokenizer.key_word.downcase.to_s} </keyword>")
 
-    @tokenizer.has_more_tokens? && @tokenizer.advance
+    advance
     @output.puts("<keyword> #{@tokenizer.key_word.downcase.to_s} </keyword>")
 
-    @tokenizer.has_more_tokens? && @tokenizer.advance
+    advance
     @output.puts("<identifier> #{@tokenizer.identifier} </identifier>")
 
-    @tokenizer.has_more_tokens? && @tokenizer.advance
+    advance
     @output.puts("<symbol> #{@tokenizer.symbol} </symbol>")
     @output.puts("</classVarDec>")
   end
@@ -47,18 +47,18 @@ class CompilationEngine
 
     @output.puts("<keyword> #{@tokenizer.key_word.downcase.to_s} </keyword>")
 
-    @tokenizer.has_more_tokens? && @tokenizer.advance
+    advance
     @output.puts("<keyword> #{@tokenizer.key_word.downcase.to_s} </keyword>")
 
-    @tokenizer.has_more_tokens? && @tokenizer.advance
+    advance
     @output.puts("<identifier> #{@tokenizer.identifier} </identifier>")
 
-    @tokenizer.has_more_tokens? && @tokenizer.advance
+    advance
     @output.puts("<symbol> #{@tokenizer.symbol} </symbol>")
 
     compile_parameter_list
 
-    @tokenizer.has_more_tokens? && @tokenizer.advance
+    advance
     @output.puts("<symbol> #{@tokenizer.symbol} </symbol>")
 
     compile_subroutine_body
@@ -73,13 +73,17 @@ class CompilationEngine
 
   private
 
+  def advance
+    @tokenizer.has_more_tokens? && @tokenizer.advance
+  end
+
   def compile_subroutine_body
     @output.puts("<subroutineBody>")
 
-    @tokenizer.has_more_tokens? && @tokenizer.advance
+    advance
     @output.puts("<symbol> #{@tokenizer.symbol} </symbol>")
 
-    @tokenizer.has_more_tokens? && @tokenizer.advance
+    advance
     @output.puts("<symbol> #{@tokenizer.symbol} </symbol>")
 
     @output.puts("</subroutineBody>")
