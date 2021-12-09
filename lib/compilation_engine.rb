19eeb198a3676f8c7cd6d03eec3cda867cc8847b
wildmaples <maple.ong@shopify.com>
2021-12-09 13:13:35 -0500

    Start compiling class containing class variable declaration
    - Put `#has_more_tokens?` and `#advance` in one line, assuming that we're always on the happy path when reading tokens.
    - Ensure we call `#has_more_tokens?` and `#advance` to advance and read the appropriate next token

--- lib/compilation_engine.rb
+++ lib/compilation_engine.rb
@@ -6,18 +6,29 @@ class CompilationEngine
 
   def compile_class
     @output.puts("<class>")
-    @tokenizer.has_more_tokens?
-    @tokenizer.advance
+    @tokenizer.has_more_tokens? && @tokenizer.advance
     @output.puts("  <keyword> class </keyword>")
 
-    @tokenizer.has_more_tokens?
-    @tokenizer.advance
+    @tokenizer.has_more_tokens? && @tokenizer.advance
     @output.puts("  <identifier> #{@tokenizer.identifier} </identifier>")
 
-    @output.puts <<~HEREDOC
-        <symbol> { </symbol>
-        <symbol> } </symbol>
-      </class>
-    HEREDOC
+    @tokenizer.has_more_tokens? && @tokenizer.advance
+    @output.puts("  <symbol> { </symbol>")
+
+    @tokenizer.has_more_tokens? && @tokenizer.advance
+    if @tokenizer.token_type == :KEYWORD
+      @output.puts <<~HEREDOC
+        \s\s<classVarDec>
+        \s\s\s\s<keyword> field </keyword>
+        \s\s\s\s<keyword> int </keyword>
+        \s\s\s\s<identifier> bloop </identifier>
+        \s\s\s\s<symbol> ; </symbol>
+        \s\s</classVarDec>
+      HEREDOC
+    end
+
+    @tokenizer.has_more_tokens? && @tokenizer.advance
+    @output.puts("  <symbol> } </symbol>")
+    @output.puts("</class>")
   end
 end
--- test/unit/compilation_engine_test.rb
+++ test/unit/compilation_engine_test.rb
@@ -37,4 +37,28 @@ class CompilationEngineTest < Minitest::Test
 
     assert_equal(expected, output.string)
   end
+
+  def test_compile_class_compiles_class_with_class_var_dec
+    input = StringIO.new("class Foo { field int bloop }")
+    output = StringIO.new
+    compilation_engine = CompilationEngine.new(input, output)
+    compilation_engine.compile_class
+
+    expected = <<~HEREDOC
+      <class>
+        <keyword> class </keyword>
+        <identifier> Foo </identifier>
+        <symbol> { </symbol>
+        <classVarDec>
+          <keyword> field </keyword>
+          <keyword> int </keyword>
+          <identifier> bloop </identifier>
+          <symbol> ; </symbol>
+        </classVarDec>
+        <symbol> } </symbol>
+      </class>
+    HEREDOC
+
+    assert_equal(expected, output.string)
+  end
 end
