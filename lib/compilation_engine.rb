d1726980b56302e4d2b64ed81830d42d52e48b83
wildmaples <maple.ong@shopify.com>
2021-12-10 10:49:45 -0500

    Use helpers to advance and print tokens

--- lib/compilation_engine.rb
+++ lib/compilation_engine.rb
@@ -1,19 +1,18 @@
+require 'cgi'
+
 class CompilationEngine
   def initialize(input, output, tokenizer: JackTokenizer.new(input))
     @output = output
     @tokenizer = tokenizer
   end
 
+  attr_reader :tokenizer
+
   def compile_class
     @output.puts("<class>")
-    advance
-    @output.puts("<keyword> class </keyword>")
-
-    advance
-    @output.puts("<identifier> #{@tokenizer.identifier} </identifier>")
-
-    advance
-    @output.puts("<symbol> #{@tokenizer.symbol} </symbol>")
+    advance_and_output_token
+    advance_and_output_token
+    advance_and_output_token
 
     advance
     if [:STATIC, :FIELD].include?(@tokenizer.key_word)
@@ -22,45 +21,32 @@ class CompilationEngine
       compile_subroutine
     end
 
-    advance
-    @output.puts("<symbol> #{@tokenizer.symbol} </symbol>")
+    advance_and_output_token
     @output.puts("</class>")
   end
 
   def compile_class_var_dec
     @output.puts("<classVarDec>")
-    @output.puts("<keyword> #{@tokenizer.key_word.downcase.to_s} </keyword>")
+    output_token
 
-    advance
-    @output.puts("<keyword> #{@tokenizer.key_word.downcase.to_s} </keyword>")
-
-    advance
-    @output.puts("<identifier> #{@tokenizer.identifier} </identifier>")
-
-    advance
-    @output.puts("<symbol> #{@tokenizer.symbol} </symbol>")
+    advance_and_output_token
+    advance_and_output_token
+    advance_and_output_token
     @output.puts("</classVarDec>")
   end
 
   def compile_subroutine
     @output.puts("<subroutineDec>")
 
-    @output.puts("<keyword> #{@tokenizer.key_word.downcase.to_s} </keyword>")
+    output_token
 
-    advance
-    @output.puts("<keyword> #{@tokenizer.key_word.downcase.to_s} </keyword>")
-
-    advance
-    @output.puts("<identifier> #{@tokenizer.identifier} </identifier>")
-
-    advance
-    @output.puts("<symbol> #{@tokenizer.symbol} </symbol>")
+    advance_and_output_token
+    advance_and_output_token
+    advance_and_output_token
 
     compile_parameter_list
 
-    advance
-    @output.puts("<symbol> #{@tokenizer.symbol} </symbol>")
-
+    advance_and_output_token
     compile_subroutine_body
 
     @output.puts("</subroutineDec>")
@@ -73,18 +59,42 @@ class CompilationEngine
 
   private
 
+  def advance_and_output_token
+    advance
+    output_token
+  end
+
   def advance
     @tokenizer.has_more_tokens? && @tokenizer.advance
+  end
+
+  def output_token
+    case tokenizer.token_type
+    when :STRING_CONST
+      token = tokenizer.string_val
+      token_type = "stringConstant"
+    when :INT_CONST
+      token = tokenizer.int_val.to_s
+      token_type = "integerConstant"
+    when :KEYWORD
+      token = tokenizer.key_word.downcase.to_s
+      token_type = "keyword"
+    when :IDENTIFIER
+      token = tokenizer.identifier
+      token_type = "identifier"
+    when :SYMBOL
+      token = tokenizer.symbol
+      token_type = "symbol"
+    end
+
+    @output.puts("<#{token_type}> #{CGI.escapeHTML(token)} </#{token_type}>")
   end
 
   def compile_subroutine_body
     @output.puts("<subroutineBody>")
 
-    advance
-    @output.puts("<symbol> #{@tokenizer.symbol} </symbol>")
-
-    advance
-    @output.puts("<symbol> #{@tokenizer.symbol} </symbol>")
+    advance_and_output_token
+    advance_and_output_token
 
     @output.puts("</subroutineBody>")
   end
