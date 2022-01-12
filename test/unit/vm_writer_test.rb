99db6163c22a141b5ec4c101a20286725b27eb9d
Maple Ong <maple.ong@shopify.com>
2022-01-12 14:30:08 -0500

    Create a setup method to reduce future lines of code written

--- test/unit/vm_writer_test.rb
+++ test/unit/vm_writer_test.rb
@@ -2,11 +2,13 @@ require "test_helper"
 require "vm_writer"
 
 class VMWriterTest < Minitest::Test
-  def test_write_function
-    output = StringIO.new
-    vm_writer = VMWriter.new(output)
-    vm_writer.write_function("Main.foo", 3)
+  def setup
+    @output = StringIO.new
+    @vm_writer = VMWriter.new(@output)
+  end
 
-    assert_equal("function Main.foo 3", output.string)
+  def test_write_function
+    @vm_writer.write_function("Main.foo", 3)
+    assert_equal("function Main.foo 3", @output.string)
   end
 end
