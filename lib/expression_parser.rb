ec06064efc7e098157a01b131180fd74c6118bb5
Maple Ong <maple.ong@shopify.com>
2022-02-15 13:39:34 -0500

    Compile less than operation

--- lib/expression_parser.rb
+++ lib/expression_parser.rb
@@ -12,6 +12,8 @@ ArithmeticOp = Struct.new(:operator, :left, :right) do
       vm_writer.write_call("Math.multiply", 2)
     when ">"
       vm_writer.write_arithmetic(:GT)
+    when "<"
+      vm_writer.write_arithmetic(:LT)
     when "="
       vm_writer.write_arithmetic(:EQ)
     when "&"
